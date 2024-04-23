67dd914046e9c448b94722a721b79c0c1032b537
Atharva Kadam <72775534+AtharvaK123@users.noreply.github.com>
2024-04-23 16:36:33 +0000

    Adding all the code

--- index.py
+++ index.py
@@ -1,3 +1,64 @@
-item = 'hi'
+def add (first, second):
+    try:
+        print('Here is your answer: ' + str(first + second))
+    except: 
+        print('You entered something wrong!')
 
-print(hi)
+def subtract (first, second):
+    try:
+        print('Here is your answer: ' + str(first - second))
+    except: 
+        print('You entered something wrong!')
+    
+def multiply (first, second):
+    try:
+        print('Here is your answer: ' + str(first * second))
+    except: 
+        print('You entered something wrong!')
+
+def divide (first, second):
+    try:
+        print('Here is your answer: ' + str(first / second))
+    except: 
+        print('You entered something wrong!')
+
+def abs (first, second):
+    try:
+        print('Here is your answer: ' + str(first ** second))
+    except: 
+        print('You entered something wrong!')
+
+ans = 'y'
+
+while(ans == 'y'):
+    ans = input('Proceed? y/n ')
+    
+    if ans == 'n':
+        break
+
+    first = input('Enter your first number here >> ')
+    type = input('What is your operand >> ')
+    second = input('Enter your second number here >> ')
+
+    try: 
+        if(type == '+'):
+            first = int(first)
+            second = int(second)
+            add(first, second)
+        elif(type == '-'):
+            first = int(first)
+            second = int(second)
+            subtract(first, second);
+        elif(type == '*'):
+            first = int(first)
+            second = int(second)
+            multiply(first, second);
+        elif(type == '/'):
+            first = int(first)
+            second = int(second)
+            divide(first, second);
+        elif(type == '^'):
+            first = int(first)
+            second = int(second)
+            abs(first, second);
+    except: print('You entered something wrong')
